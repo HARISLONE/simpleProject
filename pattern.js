56b4756c5d3b275074a513aaac897f643d689945
HARISLONE <harislone0@gmail.com>
2024-11-19 01:52:21 +0530

    Added Pattern Print Program

---
+++ pattern.js
@@ -0,0 +1,14 @@
+// pattern printing codes
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+
+let n = 4;
+for(let i = 1; i<=n;i++){
+    let row = "";
+    for(let j = 1; j<=i;j++){
+        row += j + " ";
+    }
+    console.log(row);
+}
